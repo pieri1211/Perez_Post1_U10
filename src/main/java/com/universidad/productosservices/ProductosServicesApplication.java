e9ab53723400149567b259216d31d7f8781e7260
pieri1211 <mariapierina12@hotmail.com>
2026-05-12 11:16:03 -0500

    Initial commit

---
+++ src/main/java/com/universidad/productosservices/ProductosServicesApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.productosservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductosServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/universidad/productosservices/ProductosServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.productosservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
